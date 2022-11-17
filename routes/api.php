443a21e7c620b0aa81314308c7639ebdb1abd57d
Yurec <yurecblinovgelarm2gmail.com>
2022-11-17 21:09:53 +0300

    удалось ннастроить авторизацию

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('posts/getall', 'App\Http\Controllers\Posts\PostsController@getAll');
 Route::get('posts/{id}', 'App\Http\Controllers\Posts\PostsController@getById');
 Route::post('posts/save', 'App\Http\Controllers\Posts\PostsController@savePost');
+
+// Route::group([
+//     'prefix' => 'auth'
+// ], function () {
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('registration', 'App\Http\Controllers\AuthController@registration');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+// });
