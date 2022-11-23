f02930a3efa24562372d02b6d2dd6bcfee5c383f
Yurec <yurecblinovgelarm2gmail.com>
2022-11-23 21:24:45 +0300

    auth go

--- app/Http/Controllers/Posts/PostsController.php
+++ app/Http/Controllers/Posts/PostsController.php
@@ -6,16 +6,33 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Posts;
 
+
 class PostsController extends Controller
 {
+    public function checkAuth() {
+        try {
+            $user = auth()->userOrFail();
+        } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+            return response()->json(['error' => true, 'message' => $e->getMessage()], 401);
+        }
+    }
+
     public function getAll() {
         return response()->json(Posts::get(), 200);
     }
     public function getById($id) {
-        return response()->json(Posts::find($id), 200);
+        if ($this->checkAuth()) {
+            return $this->checkAuth();
+        } else {
+            return response()->json(Posts::find($id), 200);
+        }
     }
     public function savePost(Request $req) {
-        $post = Posts::create($req->all());
-        return response()->json($post, 201);
+        if ($this->checkAuth()) {
+            return $this->checkAuth();
+        } else {
+            $post = Posts::create($req->all());
+            return response()->json($post, 201);
+        }
     }
 }
---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    
+    public function handle(Request $request, Closure $next)
+    {
+        // header('Access-Control-Allow-Origin: *');
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT')
+            ->header('Access-Control-Max-Age', '3600')
+            ->header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, X-Requested-With');
+    }
+}
